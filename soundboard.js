76508d441637b9d42ca95f27c4e76849f1a7ef9a
shaylantucker <62247890+shaylantucker@users.noreply.github.com>
2020-11-15 18:16:10 -0500

    Add files via upload

---
+++ soundboard.js
@@ -0,0 +1,15 @@
+<script>
+    alert('Pick a Sound!')
+</script>
+
+const myHeading = document.sounds('h1');
+myHeading.text= 'Choose a Sound!';
+
+myVariable = "Ride";
+alert('sound!')
+
+myVariable = "Crash";
+alert('sound!')
+
+myVariable = "Splash";
+alert('sound!')
